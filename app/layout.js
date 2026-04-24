e9449c15623eef89cb552cf4df1fd99d9dc25b12
Shin19960717 <a0936085717@gmail.com>
2026-04-24 14:49:42 +0800

    update

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 export const metadata = {
   metadataBase: new URL("https://taipeiwildspa.com"),
 
-  title: "Taipei Wild Spa｜台北同志按摩｜Taipei gay Spa｜Taipei gay massage",
+  title: "Taipei Wild Spa｜臺北 野Spa｜台北同志按摩｜Taipei gay Spa｜Taipei gay massage｜타이베이 게이 마사지",
   description:
     "Taipei Wild Spa 提供台北同志按摩，重視隱私與舒適體驗，打造安心放鬆空間，並且我們致力讓身材最好最帥的師傅為您服務。Gay massage in Taipei with private rooms.",
 
